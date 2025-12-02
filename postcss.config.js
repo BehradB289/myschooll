163f315768287004f4b40319fa15632333f3ef29
BehradB289 <behradb2xbox@gmail.com>
2025-12-02 20:52:27 +0330

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: {
+    // TailwindCSS را به PostCSS معرفی می‌کند
+    tailwindcss: {},
+    // Autoprefixer را برای سازگاری مرورگرها معرفی می‌کند
+    autoprefixer: {},
+  },
+}
