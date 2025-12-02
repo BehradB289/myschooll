ffa9d7e343aa794a8a97f7ab83c4f19626ea05f2
BehradB289 <behradb2xbox@gmail.com>
2025-12-02 21:11:48 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// تنظیمات ویت برای پشتیبانی از ویژگی‌های جدید جاوا‌اسکریپت
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext' // استفاده از آخرین نسخه جاوا‌اسکریپت برای بیلد نهایی
+  },
+  esbuild: {
+    target: 'esnext' // رفع خطای import.meta در محیط توسعه و پیش‌نمایش
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
 })
